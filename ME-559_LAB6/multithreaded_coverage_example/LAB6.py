8d8bf56af48dc0def3969aa395cc47cbce38dc7e
funkeymantic <martincpaul2000@gmail.com.com>
2024-12-02 13:33:27 -0800

    Lab6 done

--- ME-559_LAB6/multithreaded_coverage_example/LAB6.py
+++ ME-559_LAB6/multithreaded_coverage_example/LAB6.py
@@ -1,4 +1,6 @@
 import rclpy
+import random
+import time
 from rclpy.node import Node
 from rclpy.action.client import ActionClient
 from rclpy.qos import qos_profile_sensor_data
@@ -14,108 +16,192 @@ from pynput.keyboard import KeyCode
 from key_commander import KeyCommander
 from threading import Lock
 from rclpy.executors import MultiThreadedExecutor
-import random
 
-# Lock for multithreading
+
+# To help with Multithreading
 lock = Lock()
 
 class Slash(Node):
+    """
+    Class to coordinate actions and subscriptions
+    """
+
     def __init__(self, namespace):
         super().__init__('slasher')
 
-        # Callback Groups
-        cb_Subscription = MutuallyExclusiveCallbackGroup()
-        cb_Action = MutuallyExclusiveCallbackGroup()
+        # 2 Seperate Callback Groups for handling the bumper Subscription and Action Clients
+        cb_Subscripion = MutuallyExclusiveCallbackGroup()
+        cb_Action =MutuallyExclusiveCallbackGroup()
 
-        # Subscriptions
+        # Subscription to Hazards, the callback function attached only looks for bumper hits
         self.subscription = self.create_subscription(
-            HazardDetectionVector, f'/{namespace}/hazard_detection',
-            self.listener_callback, qos_profile_sensor_data,
-            callback_group=cb_Subscription
-        )
+            HazardDetectionVector, f'/{namespace}/hazard_detection', self.listener_callback, qos_profile_sensor_data,callback_group=cb_Subscripion)
 
-        # Action Clients
-        self._undock_ac = ActionClient(self, Undock, f'/{namespace}/undock', callback_group=cb_Action)
-        self._drive_ac = ActionClient(self, DriveDistance, f'/{namespace}/drive_distance', callback_group=cb_Action)
-        self._rotate_ac = ActionClient(self, RotateAngle, f'/{namespace}/rotate_angle', callback_group=cb_Action)
-        self._dock_ac = ActionClient(self, Dock, f'/{namespace}/dock', callback_group=cb_Action)
+        # Action clients for movements
+        self._undock_ac = ActionClient(self, Undock, f'/{namespace}/undock',callback_group=cb_Action)
+        self._dock_ac = ActionClient(self, Dock, f'/{namespace}/dock',callback_group=cb_Action)
+        self._drive_ac = ActionClient(self, DriveDistance, f'/{namespace}/drive_distance',callback_group=cb_Action)
+        self._rotate_ac = ActionClient(self, RotateAngle, f'/{namespace}/rotate_angle',callback_group=cb_Action)
 
         # Variables
         self._goal_uuid = None
 
-    def send_action_goal(self, action_client, goal):
-        with lock:
-            handle = action_client.send_goal_async(goal)
-            while not handle.done():
-                pass
-            self._goal_uuid = handle.result()
-        
-        while self._goal_uuid is not None and self._goal_uuid.status in (GoalStatus.STATUS_UNKNOWN, GoalStatus.STATUS_ACCEPTED):
-            pass
-        
-        self._goal_uuid = None
 
-    def undock(self):
-        self._undock_ac.wait_for_server()
-        goal = Undock.Goal()
-        self.send_action_goal(self._undock_ac, goal)
-        self.get_logger().info("Robot undocked successfully.")
-
-    def drive_random_distance(self):
-        distance = random.uniform(1.0, 4.0)
-        self._drive_ac.wait_for_server()
-        goal = DriveDistance.Goal()
-        goal.distance = distance
-        self.send_action_goal(self._drive_ac, goal)
-        self.get_logger().info(f"Robot drove {distance:.2f} meters.")
-
-    def rotate_random_angle(self):
-        angle = random.uniform(90.0, 270.0)
-        self._rotate_ac.wait_for_server()
-        goal = RotateAngle.Goal()
-        goal.angle = angle
-        self.send_action_goal(self._rotate_ac, goal)
-        self.get_logger().info(f"Robot rotated {angle:.2f} degrees.")
-
-    def dock(self):
-        self._dock_ac.wait_for_server()
-        goal = Dock.Goal()
-        self.send_action_goal(self._dock_ac, goal)
-        self.get_logger().info("Robot docked successfully.")
-
-    def execute(self):
-        self.undock()
-        for _ in range(2):
-            self.drive_random_distance()
-            self.rotate_random_angle()
-        self.dock()
 
     def listener_callback(self, msg):
+        '''
+        This function is called every time self.subscription gets a message
+        from the Robot. Here it parses the message from the Robot and if its
+        a 'bump' message, cancel the current action. 
+
+        For this to work, make sure you have:
+        ros__parameters:
+            reflexes_enabled: false
+        in your Application Configuration Parameters File!!!
+        '''
+
+        # If it wasn't doing anything, there's nothing to cancel
         if self._goal_uuid is None:
             return
+
+        # msg.detections is an array of HazardDetection from HazardDetectionVectors.
+        # Other types can be gotten from HazardDetection.msg
         for detection in msg.detections:
-            if detection.type == 1:  # Bump
-                self.get_logger().warning("Hazard detected. Canceling goal.")
-                with lock:
+            if detection.type == 1:   #If it is a bump
+                self.get_logger().warning('HAZARD DETECTED')
+                with lock: # Make this the only thing happening
+                    self.get_logger().warning('CANCELING GOAL')
                     self._goal_uuid.cancel_goal_async()
-                break
+                    # Loop until the goal status returns canceled
+                    while self._goal_uuid is not None and self._goal_uuid.status is not GoalStatus.STATUS_CANCELED:
+                        pass
 
 
-def main():
-    rclpy.init()
-    namespace = 'create3_05F8'
-    node = Slash(namespace)
-    executor = MultiThreadedExecutor(2)
-    executor.add_node(node)
+#--------------------Async send goal calls-----------------------------
+    def sendDriveGoal(self,goal):
+        """
+        Sends a drive goal asynchronously and 'blocks' until the goal is complete
+        """
+        
+        with lock:
+            drive_handle = self._drive_ac.send_goal_async(goal)
+            while not drive_handle.done():
+                pass # Wait for Action Server to accept goal
+            
+            # Hold ID in case we need to cancel it
+            self._goal_uuid = drive_handle.result() 
 
-    try:
-        node.get_logger().info("Starting robot sequence...")
-        node.execute()
-    except KeyboardInterrupt:
-        node.get_logger().info("Shutting down.")
-    finally:
-        rclpy.shutdown()
+        while self._goal_uuid is not None and self._goal_uuid.status == GoalStatus.STATUS_UNKNOWN:
+            pass # Wait until a Status has been assigned
 
+        # After getting goalID, Loop while the goal is currently running
+        while self._goal_uuid is not None and self._goal_uuid.status is not GoalStatus.STATUS_SUCCEEDED:
+            if self._goal_uuid is None or self._goal_uuid.status is GoalStatus.STATUS_CANCELED:
+                break # If the goal was canceled, stop looping otherwise loop till finished
+            pass
+
+        # Reset the goal ID, nothing should be running
+        self._goal_uuid = None
+
+    def sendRotateGoal(self,goal):
+        """
+        Sends a rotate goal asynchronously and 'blocks' until the goal is complete
+        """
+        
+        with lock:
+            rotate_handle = self._rotate_ac.send_goal_async(goal)
+            while not rotate_handle.done():
+                pass # Wait for Action Server to accept goal
+            
+            # Hold ID in case we need to cancel it
+            self._goal_uuid = rotate_handle.result() 
+
+        while self._goal_uuid is not None and self._goal_uuid.status == GoalStatus.STATUS_UNKNOWN:
+            pass # Wait until a Status has been assigned
+
+        # After getting goalID, Loop while the goal is currently running
+        while self._goal_uuid is not None and self._goal_uuid.status is not GoalStatus.STATUS_SUCCEEDED:
+            if self._goal_uuid is None or self._goal_uuid.status is GoalStatus.STATUS_CANCELED:
+                break # If the goal was canceled, stop looping otherwise loop till finished
+            pass
+
+        # Reset the goal ID, nothing should be running
+        self._goal_uuid = None
+
+#----------------------------------------------------------------------
+
+
+    def drive_away(self):
+        
+        # Freshly started, undock
+        self.get_logger().warning('WAITING FOR SERVER')
+    # wait until the robot server is found and ready to receive a new goal
+        self._undock_ac.wait_for_server()
+        self.get_logger().warning('SERVER AVAILABLE')
+        self.get_logger().warning('UNDOCKING')
+
+    # create new Undock goal object to send to server
+        undock_goal = Undock.Goal()
+
+        self._undock_ac.send_goal(undock_goal)
+        self.get_logger().warning('UNDOCKED')
+
+        for _ in range(2):
+        # wait for DriveDistance action server (blocking)
+            self._drive_ac.wait_for_server()
+            self.get_logger().warning('DRIVING!')
+
+        # create goal object and specify distance to drive
+            drive_goal = DriveDistance.Goal()
+            drive_goal.distance = random.uniform(1.0, 4.0)
+            print(f'Driving: {drive_goal.distance}')
+
+        # send goal to async function
+            self.sendDriveGoal(drive_goal)
+
+        # wait for RotateAngle action server (blocking)
+            self._rotate_ac.wait_for_server()
+            self.get_logger().warning('ROTATING!')
+
+        # create goal object and specify distance to drive
+            rotate_goal = RotateAngle.Goal()
+            rotate_goal.angle = random.uniform(90, 270) * (3.14159265359 / 180)
+            print(f'Rotating: {rotate_goal.angle} rad and {rotate_goal.angle / (3.14159265359 / 180)} deg.')
+
+        # send goal to async function
+            self.sendRotateGoal(rotate_goal)
+
+        # Freshly started, dock
+        self.get_logger().warning('WAITING FOR SERVER')
+    # wait until the robot server is found and ready to receive a new goal
+        self._dock_ac.wait_for_server()
+        self.get_logger().warning('SERVER AVAILABLE')
+        self.get_logger().warning('DOCKING')
+
+    # create new Undock goal object to send to server
+        dock_goal = Dock.Goal()
+
+        self._dock_ac.send_goal(dock_goal)
+        self.get_logger().warning('DOCKED')
 
 if __name__ == '__main__':
-    main()
+
+    rclpy.init()
+
+    namespace = 'create3_05F8'
+    s = Slash(namespace)
+
+    # 1 thread for the Subscription, another for the Action Clients
+    exec = MultiThreadedExecutor(2)
+    exec.add_node(s)
+
+    keycom = KeyCommander([
+        (KeyCode(char='r'), s.drive_away),
+        ])
+
+    print("r: Start drive_away")
+    try:
+        exec.spin() # execute slash callbacks until shutdown or destroy is called
+    except KeyboardInterrupt:
+        s.get_logger().info('KeyboardInterrupt, shutting down.\n')
+    rclpy.shutdown()
